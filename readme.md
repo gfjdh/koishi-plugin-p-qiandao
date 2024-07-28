5166b301751ffacbe2c25f65fdc822fae573f85c
gfjdh1 <2556123498@qq.com>
2024-07-28 15:06:24 +0800

    message

--- readme.md
+++ readme.md
@@ -3,3 +3,17 @@
 [![npm](https://img.shields.io/npm/v/koishi-plugin-qiandao?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-qiandao)
 
 p点-签到
+
+- **指令：p-query**
+查询p点
+别名：查询p点,p点查询
+
+- **指令：p-sign**
+签到
+别名：签到
+管理员可以无限签到
+
+- **指令：p-transfer [target] [value:number]**
+对签到过的用户转账p点
+别名：转账
+目标用户可以使用@元素或qq号，管理员可以转账负数（制裁用）
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,173 @@
-import { Context, Schema } from 'koishi'
+import { Context, h, Schema, Time } from 'koishi'
 
 export const name = 'qiandao'
 
-export interface Config {}
+export const usage = `
+- **指令：p-query**\n
+    别名：查询p点,p点查询\n
+- **指令：p-sign**\n
+    别名：签到\n
+    管理员可以无限签到\n
+- **指令：p-transfer [target] [value:number]**\n
+    别名：转账\n
+    目标用户可以使用@元素或qq号，管理员可以转账负数（制裁用）\n
+  在本地化中可以编辑问候语，按时段从三句中随机一句
+`
 
-export const Config: Schema<Config> = Schema.object({})
+export const inject = {
+  required: ['database'],
+  optional: [],
+}
+export interface Config {
+  adminUsers: string[]
+  init_p: number
+  limit_p: number
+  upper_limit: number
+  lower_limit: number
+  outputLogs: boolean
+}
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export const Config: Schema<Config> = Schema.object({
+  adminUsers: Schema.array(Schema.string()),
+  init_p: Schema.number().default(1000),
+  limit_p: Schema.number().default(99999),
+  upper_limit: Schema.number().default(1000),
+  lower_limit: Schema.number().default(500),
+  outputLogs: Schema.boolean().default(true),
+}).i18n({
+  'zh-CN': require('./locales/zh-CN'),
+})
+
+function mathRandomInt(a: number, b: number) {
+  if (a > b) {      // Swap a and b to ensure a is smaller.
+    var c = a; a = b; b = c;
+  } return Math.floor(Math.random() * (b - a + 1) + a);
+}
+
+async function isTargetIdExists(ctx: Context, USERID: string) {
+  //检查数据表中是否有指定id者
+  const targetInfo = await ctx.database.get('p_system', { userid: USERID });
+  return targetInfo.length == 0;
+}
+declare module 'koishi' {
+  interface Tables { p_system: p_system }
+}
+export interface p_system {
+  id: number
+  userid: string
+  usersname: string
+  p: number
+  time: Date
+}
+
+export async function apply(ctx: Context, cfg: Config) {
+  ctx.model.extend('p_system', {
+    id: 'unsigned',
+    userid: 'string',
+    time: 'timestamp',
+    usersname: 'string',
+    p: 'integer'
+  }, { autoInc: true })
+
+  const logger = ctx.logger("p-签到")
+  ctx.i18n.define('zh-CN', require('./locales/zh-CN'))
+
+  ctx.command('p/p-query').alias('查询p点', 'p点查询').action(async ({ session }) => {
+    const USERID = session.userId;//发送者的用户id
+    const notExists = await isTargetIdExists(ctx, USERID); //该群中的该用户是否签到过
+    if (notExists) return session.text('.account-notExists');
+    const usersdata = await ctx.database.get('p_system', { userid: USERID });
+    const saving = usersdata[0]?.p;
+    await session.sendQueued(session.text('.result', [saving]));
+  });
+
+  ctx.command('p/p-sign').alias('签到').action(async ({ session }) => {
+    if ((session.userId) == null) return session.text('.sign-failed');
+    const USERNAME = session.username; //发送者的用户名
+    const USERID = session.userId;     //发送者的用户id
+    const notExists = await isTargetIdExists(ctx, USERID); //该群中的该用户是否签到过
+    if (notExists) {
+      await ctx.database.create('p_system', { userid: USERID, usersname: USERNAME, p: cfg.init_p, time: new Date() })
+      await session.sendQueued(session.text('.new-user', [cfg.init_p]));
+    }
+    await ctx.database.set('p_system', { userid: USERID }, { usersname: USERNAME })//更新用户名
+
+    const usersdata = await ctx.database.get('p_system', { userid: USERID });
+    const saving = usersdata[0].p;
+    if (saving >= 99999) return session.text('.exceeding-limit', [cfg.limit_p]);
+
+    let oldDate: string;
+    if (usersdata[0]?.time) oldDate = Time.template('yyyy-MM-dd', usersdata[0].time);
+    const newDate = Time.template('yyyy-MM-dd', new Date());
+
+    let bonus = Number(mathRandomInt(cfg.lower_limit, cfg.upper_limit));
+
+    if (cfg.adminUsers.includes(USERID) || newDate != oldDate || notExists) //管理员||新日期||新用户
+    {
+      await ctx.database.set('p_system', { userid: USERID }, { time: new Date() }) //更新签到时间
+      await session.sendQueued(`${h('at', { id: USERID })}.\n${session.text('.sign-succeed', [bonus])}\n${session.text('.balance-show', [bonus + saving])}`);
+
+      await ctx.database.set('p_system', { userid: USERID }, { p: Number(saving + bonus) }) //更新余额
+
+      if (ctx.database.get('p_system', { userid: USERID })[0]?.p > cfg.limit_p)
+        await ctx.database.set('p_system', { userid: USERID }, { p: cfg.limit_p })
+      const hour = Number((Time.template('hh', new Date())));
+      const x = mathRandomInt(0, 2);
+      const timeRanges = [
+        { start: 0, end: 2, label: '23-2' },
+        { start: 2, end: 6, label: '2-6' },
+        { start: 6, end: 8, label: '6-8' },
+        { start: 8, end: 11, label: '8-11' },
+        { start: 11, end: 17, label: '11-17' },
+        { start: 17, end: 19, label: '17-19' },
+        { start: 19, end: 21, label: '19-21' },
+        { start: 21, end: 23, label: '21-23' },
+        { start: 23, end: 24, label: '23-2' }
+      ];
+
+      let url = '.greating-';
+      const currentRange = timeRanges.find(range => hour >= range.start && hour < range.end);
+      url += currentRange.label + '.' + x;
+
+      await session.send(session.text(url));
+      if (cfg.outputLogs) logger.success(USERID + USERNAME + '完成了一次签到！');
+    }
+    else {
+      await session.sendQueued(h('at', { id: USERID }) + session.text('.already-signed'));
+      if (cfg.outputLogs) logger.info(USERID + USERNAME + '该用户已经签到');
+    }
+  });
+
+  ctx.command('p/p-transfer [target] [value:number]').alias('转账').action(async ({ session }, target, value) => {
+    if ((target) == null) { return '“转账 @对方 转账额度”'; }
+    const USERID = session.userId;//发送者的用户id
+    const notExists = await isTargetIdExists(ctx, USERID); //用户是否签到过
+    if (notExists) return session.text('.account-notExists');
+    const usersdata = await ctx.database.get('p_system', { userid: USERID });
+    var targetid = target;
+    const p = Number(value);
+    let text = session.elements.filter((element) => element.type == 'at');
+    let regex = /\b([1-9]\d{6,})\b/;
+    let match = regex.exec(String(target));
+    if (!(text.length === 0) || !match) {
+      if (!match) return session.text('.no-id');
+      targetid = String(text.map(element => element.attrs.id)); // 提取 @ 元素的 id
+    }
+
+    const targetNotExists = await isTargetIdExists(ctx, targetid); //目标用户是否签到过
+    if (targetNotExists) return session.text('.target-notExists');
+    const targetData = await ctx.database.get('p_system', { userid: targetid });
+    if (USERID == targetid) return session.text('.self-target');
+    if (!(p > 0) && !cfg.adminUsers.includes(USERID)) return session.text('.wrong-value'); //管理员可转负数
+    if (usersdata[0].p < p) return session.text('.no-enough-p');
+
+    // 更新发送者和接收者的余额
+    await ctx.database.set('p_system', { userid: USERID }, { p: usersdata[0].p - p });
+    await ctx.database.set('p_system', { userid: targetid }, { p: targetData[0].p + p });
+
+    const senderBalance = (await ctx.database.get('p_system', { userid: USERID }))[0].p;
+    const receiverBalance = (await ctx.database.get('p_system', { userid: targetid }))[0].p;
+
+    return `${session.text('.transfer-succeed')}\n.${session.text('.result', [targetid,receiverBalance])}\n.${session.text('.result', [USERID,senderBalance])}`;
+  });
 }
